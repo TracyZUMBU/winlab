92990167b4874e8c5c890d7dd53b6b150866e2f4
tracyzumbu <tracyzumbu@lilo.org>
2026-05-19 16:45:53 +0200

    chore(app): update version and build numbers for mobile app
    - Bumped version to 1.1.1 in both app.config.js and package.json.
    - Updated build number to 6 for iOS and version code to 5 for Android to reflect the latest changes.

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -34,7 +34,7 @@ module.exports = {
   expo: {
     name: getAppName(),
     slug: "mobile",
-    version: "1.1.0",
+    version: "1.1.1",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "mobile",
@@ -43,7 +43,7 @@ module.exports = {
     ios: {
       supportsTablet: true,
       bundleIdentifier: getBundleIdentifier(),
-      buildNumber: "5",
+      buildNumber: "6",
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
         NSPhotoLibraryUsageDescription:
@@ -52,7 +52,7 @@ module.exports = {
       googleServicesFile: getGoogleServicesPlist(),
     },
     android: {
-      versionCode: 4,
+      versionCode: 5,
       adaptiveIcon: {
         backgroundColor: "#E6F4FE",
         foregroundImage: "./assets/images/android-icon-foreground.png",
